3e6e4cd50b7588228662e2146632fd61ff5f6fde
Tasos Kakouris <tasoskakour@gmail.com>
2022-11-12 08:30:24 +0200

    Initial Bootstrap

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    extends: [
+        "airbnb",
+        "plugin:react/recommended",
+        "airbnb/base",
+        // "plugin:unicorn/recommended",
+        "prettier",
+        "prettier/prettier",
+        "plugin:prettier/recommended",
+    ],
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    parserOptions: {
+        // ecmaFeatures: {
+        //     jsx: true,
+        // },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["react", "react-hooks"],
+    rules: {
+        "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+        // your overrides
+    },
+};
